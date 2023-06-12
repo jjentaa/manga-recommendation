e11d0abaf4ccca336cac778e0f0585399660a568
_MadNess <86233076+jjentaa@users.noreply.github.com>
2023-06-12 12:40:07 +0700

    Update 04_give-feedback.py

--- pages/04_give-feedback.py
+++ pages/04_give-feedback.py
@@ -3,22 +3,22 @@ import pandas as pd
 
 st.title("Give Feedback")
 
-def cal_avg_score():
-    fb_df = pd.read_csv("feedback.csv")
-    total = fb_df['score'].sum()
-    nn = len(fb_df["score"])
-    return round(total/nn, 2)
+# def cal_avg_score():
+#     fb_df = pd.read_csv("feedback.csv")
+#     total = fb_df['score'].sum()
+#     nn = len(fb_df["score"])
+#     return round(total/nn, 2)
 
-def get_num_usr():
-    with open('counter.txt') as f:
-        contents = f.read()
-        return int(contents)
+# def get_num_usr():
+#     with open('counter.txt') as f:
+#         contents = f.read()
+#         return int(contents)
 
-avg_scr = cal_avg_score()
-usr_cnt = get_num_usr()
-col1, col2 = st.columns(2)
-col1.metric('Score', avg_scr)
-col2.metric('User used', usr_cnt)
+# avg_scr = cal_avg_score()
+# usr_cnt = get_num_usr()
+# col1, col2 = st.columns(2)
+# col1.metric('Score', avg_scr)
+# col2.metric('User used', usr_cnt)
 
 score = st.slider('How good', 0, 10, 1)
 ffeedback = st.text_input('Pls enter some feedback :')
